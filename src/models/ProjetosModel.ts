8f4729bdf7a14b9ca4b0e9de2de8895af051aa47
Lucas Luz <lucasfelipeluz.dev@gmail.com>
2022-12-12 22:15:42 -0300

    feature/Models - implementação das tipagens. - implementação de models seguindo o padrão do banco de dados.

---
+++ src/models/ProjetosModel.ts
@@ -0,0 +1,19 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '@src/config/db';
+import { Projetos } from '@src/types/Models';
+
+class ProjetosModel extends Model<Projetos> {}
+
+ProjetosModel.init(
+  {
+    id: { type: DataTypes.NUMBER, allowNull: false, primaryKey: true, autoIncrement: true },
+    titulo: { type: DataTypes.STRING, allowNull: false },
+    descricao: { type: DataTypes.STRING, allowNull: false },
+    prioridade: { type: DataTypes.NUMBER, allowNull: false },
+    url_github: { type: DataTypes.STRING, allowNull: false },
+    url_website: { type: DataTypes.STRING, allowNull: false },
+  },
+  { sequelize, modelName: 'projetos', createdAt: 'criado_em', updatedAt: 'alterado_em' },
+);
+
+export default ProjetosModel;
