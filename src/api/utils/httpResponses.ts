a2b1e3f308a202915faa07f83466dce62aeb9922
Lucas F Luz <lucasfelipeluz.dev@gmail.com>
2024-06-25 21:34:59 -0300

    chore: transalated documentations

--- src/api/utils/httpResponses.ts
+++ src/api/utils/httpResponses.ts
@@ -2,12 +2,13 @@ import strings from '@/domain/utils/strings';
 import { Response } from 'express';
 
 /**
- * `200`- A solicitação foi bem-sucedida. O significado do resultado de "sucesso"
- * depende do método HTTP:
- * - GET: o recurso foi buscado e transmitido no corpo da mensagem.
- * - HEAD: os cabeçalhos de representação são incluídos na resposta sem qualquer corpo de mensagem.
- * - PUT ou POST: O recurso que descreve o resultado da ação é transmitido no corpo da mensagem.
- * - TRACE: o corpo da mensagem contém a mensagem de solicitação recebida pelo servidor.
+ * `200` - The request was successful. The meaning of the "success" result
+ * depends on the HTTP method:
+ * - GET: The resource was fetched and transmitted in the message body.
+ * - HEAD: The representation headers are included in the response without any message body.
+ * - PUT or POST: The resource describing the result of the action is transmitted in the message
+ * body.
+ * - TRACE: The message body contains the request message received by the server.
  */
 function ok<T>(res: Response, data: T, message?: string) {
   if (data && !message) return res.status(200).send(data);
@@ -16,99 +17,99 @@ function ok<T>(res: Response, data: T, message?: string) {
 }
 
 /**
- * `201`- A solicitação foi bem-sucedida e, como resultado, um novo recurso foi criado.
- * Normalmente, esta é a resposta enviada após POST ou PUT.
+ * `201` - The request was successful and as a result, a new resource was created.
+ * Typically, this is the response sent after a POST or PUT request.
  */
 function created<T>(res: Response, data: T, message?: string) {
   return res.status(201).send({ message, data });
 }
 
 /**
- * `202`- A solicitação foi recebida, mas ainda não foi atendida. É sem compromisso,
- * pois não há como no HTTP enviar posteriormente uma resposta assíncrona
- * indicando o resultado da solicitação. Destina-se a casos em que outro
- * processo ou servidor manipula a solicitação ou processamento em lote.
+ * `202`- The request has been received but has not been acted upon yet. It is non-committal,
+ * since there is no way in HTTP to later send an asynchronous response
+ * indicating the outcome of the request. It is intended for cases where another
+ * process or server handles the request or batch processing.
  */
 function accepted<T>(res: Response, data: T) {
   return res.status(202).send({ data });
 }
 
 /**
- * `203` - Esse código de resposta significa que os metadados retornados
- * não são exatamente os mesmos que estão disponíveis no servidor de origem,
- * mas são coletados de uma cópia local ou de terceiros. Isso é usado principalmente
- * para espelhos ou backups de outro recurso. Exceto para esse caso específico,
- * a resposta 200 OK é preferida a este status.
+ * `203` - This response code means that the returned metadata
+ * is not exactly the same as available on the origin server,
+ * but is collected from a local or third-party copy. This is mainly used
+ * for mirrors or backups of another resource. Except for this specific case,
+ * the 200 OK response is preferred over this status.
  */
 function nonAuthoritativeInformation<T>(res: Response, data: T) {
   return res.status(203).send({ data });
 }
 
 /**
- * `204` - Não há conteúdo para enviar para esta solicitação, mas os cabeçalhos podem ser úteis.
- *  O agente do usuário pode atualizar seus cabeçalhos em cache para este recurso com os novos.
+ * `204` - There is no content to send for this request, but the headers may be useful.
+ * The user agent may update its cached headers for this resource with the new ones.
  */
 function noContent(res: Response) {
   return res.status(204).send();
 }
 
 /**
- * `206`- Este código de resposta é usado quando o cabeçalho Range é enviado do
- *  cliente para solicitar apenas parte de um recurso.
+ * `206` - This response code is used when the Range header is sent from the
+ * client to request only part of a resource.
  */
 function partialContent<T>(res: Response, data: T) {
   return res.status(206).send({ data });
 }
 
 /**
- * `301` - A URL do recurso solicitado foi alterada permanentemente.
- *  A nova URL é fornecida na resposta.
+ * `301` - The URL of the requested resource has been permanently changed.
+ * The new URL is provided in the response.
  */
 function movedPermanently(res: Response, newUrl: string, message?: string) {
   return res.status(301).send({ newUrl, message });
 }
 
 /**
- * `400` - Esta resposta significa que o servidor não entendeu a solicitação.
+ * `400` - This response means that the server did not understand the request.
  */
 function badRequest(res: Response, message: string) {
   return res.status(400).send({ message });
 }
 
 /**
- * `401` - Embora o padrão HTTP especifique "unauthorized", semanticamente, essa resposta
- * significa "unauthenticated". Ou seja, o cliente deve se autenticar para obter a resposta
- * solicitada.
+ * `401` - Although the HTTP standard specifies "unauthorized", semantically, this response
+ * means "unauthenticated". In other words, the client must authenticate itself to get the
+ * requested response.
  */
 function unauthorized(res: Response, message: string) {
   return res.status(401).send({ message });
 }
 
 /**
- * `403` - O cliente não tem direitos de acesso ao conteúdo, ou seja, ele está proibido.
+ * `403` - The client does not have access rights to the content, in other words, it is forbidden.
  */
 function forbidden(res: Response, message: string) {
   return res.status(403).send({ message });
 }
 
 /**
- * `404` - O servidor não pode encontrar o recurso solicitado.
+ * `404` - The server cannot find the requested resource.
  */
 function notFound(res: Response) {
   return res.status(404).send({ message: strings.urlNotFound });
 }
 
 /**
- * `405`- O método de solicitação é conhecido pelo servidor, mas foi desativado
- * e não pode ser usado.
- * Exemplo: Um cliente tenta usar um método POST em um recurso que aceita apenas solicitações GET.
+ * `405` - The request method is known by the server, but has been disabled
+ * and cannot be used.
+ * Example: A client tries to use a POST method on a resource that only accepts GET requests.
  */
 function methodNotAllowed(res: Response, message: string) {
   return res.status(405).send({ message });
 }
 
 /**
- * `500` - O servidor encontrou uma situação inesperada que o impediu de atender à solicitação.
+ * `500` - The server encountered an unexpected situation that prevented it from fulfilling the request.
  */
 function internalServerError(res: Response, message: string, error?: any) {
   console.log(error);
@@ -116,13 +117,12 @@ function internalServerError(res: Response, message: string, error?: any) {
 }
 
 /**
- * Método para lidar com erros de servidor.
+ * Method to handle server errors.
  */
 function handleServerError(res: Response, message: string, error: any) {
   if (!error.code || error.code === 500) {
     return internalServerError(res, strings.internalServerError, error);
   }
-
   return res.status(error.code).send({ message: error.message || message, error });
 }
 
