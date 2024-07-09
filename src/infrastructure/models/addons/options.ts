7b331460b02ad4a750cd9ef478fc470977d5eaa0
Lucas F Luz <lucasfelipeluz.dev@gmail.com>
2024-07-08 21:32:07 -0300

    chore: models addons

--- src/infrastructure/models/addons/options.ts
+++ src/infrastructure/models/addons/options.ts
@@ -1,10 +1,7 @@
 import strings from '@/domain/utils/strings';
 import { dbConnection } from '@/infrastructure/config/dbConnection';
 import { InitOptions } from 'sequelize';
-import ProjectModel from '../ProjectModel';
-import SkillModel from '../SkillModel';
-import ProjectSkillModel from '../ProjectSkillModel';
-import ProjectImageModel from '../ProjectImageModel';
+import { ProjectModel, SkillModel, ProjectSkillModel, ProjectImageModel } from '../';
 
 const project: InitOptions<ProjectModel> = {
   tableName: strings.project,
