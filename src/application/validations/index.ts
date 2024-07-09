87af3d48204741218561a3b26638069f0abb0a2f
Lucas F Luz <lucasfelipeluz.dev@gmail.com>
2024-07-09 16:46:30 -0300

    chore: fix validateProperties

--- src/application/validations/index.ts
+++ src/application/validations/index.ts
@@ -3,9 +3,12 @@ import ValidationError from '@/domain/errors/ValidationError';
 /**
  * Validate properties, case any property is undefined or null, will throw a ValidationError
  */
-function validateProperties<T>(context: T, properties: string[]): void {
+function validateProperties<T extends Record<string, unknown>>(
+  context: T,
+  properties: string[],
+): void {
   properties.forEach((prop) => {
-    if ((context as any)[prop] === undefined || (context as any)[prop] === null) {
+    if (context[prop] === undefined || context[prop] === null) {
       throw new ValidationError(`${prop} is required`);
     }
   });
